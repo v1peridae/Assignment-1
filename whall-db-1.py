e23d38cc5d73b91574d04142b6fd1fe5da1a3c09
louisa <82815171+v1peridae@users.noreply.github.com>
2023-10-17 03:30:17 -0700

    Create whall-db-1.py

---
+++ whall-db-1.py
@@ -0,0 +1,10 @@
+#this code is for the Wolsey Hall Database. It needs the students name, age and nationality.
+username = input("Please type in your first name to proceed. >")
+print("Hello {}.".format(username))
+#age is for the age 
+age = input("Please type in your age in numbers. >")
+print("You are {}.".format (age)) 
+#nat is for nationality
+nat = input("Please type in your nationality. >")
+print("Oh cool! You're {}.".format (nat))
+print ("Do you wish to proceed with these details? " + username + " , " + age + " , " + nat)
